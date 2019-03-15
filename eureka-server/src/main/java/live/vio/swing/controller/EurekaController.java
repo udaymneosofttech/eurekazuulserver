db92b80b92c03a4ad1b4f37c27723612f46f82d2
udaymneosofttech <uday.m@neosofttech.com>
2019-03-15 17:31:39 +0530

    eureka-serverfiles

---
+++ eureka-server/src/main/java/live/vio/swing/controller/EurekaController.java
@@ -0,0 +1,13 @@
+package live.vio.swing.controller;
+
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class EurekaController {
+
+
+}
